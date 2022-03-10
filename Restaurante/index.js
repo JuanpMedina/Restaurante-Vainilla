6ce9a1a0ee2bcb29e0c2fb5d81f17fb1a89bbc4c
Juan Pablo Medina Diaz <juampamedinadiaz2002@gmail.com>
2022-03-09 19:01:30 -0500

    -implementacion bootstrap -validacion de campos de js

--- Restaurante/index.js
+++ Restaurante/index.js
@@ -10,6 +10,13 @@ function obtenerValoresFormulario() {
     let clave = document.getElementById("txtClave").value 
     let direccion = document.getElementById("txtDireccion").value 
 
+    if(correo==""){
+        // alert("El correo es obligatorio")
+        document.getElementById("lblAlerta").style.display = ""
+        document.getElementById("lblAlerta").innerText="El correo es obligatorio."
+        return
+    }
+
     // Forma de crear un objetos
     // Inicializar un objeto con propiedades
     let usuario = {celular, "clave":clave}
@@ -24,7 +31,21 @@ function obtenerValoresFormulario() {
     // Froma resumida de crear el objeto 
     // let usuario = {celular, clave, celular, clave, direccion}
 
+
+    document.getElementById("txtCorreo").value = ""
+    document.getElementById("txtNombre").value = ""
+    document.getElementById("txtCelular").value = ""
+    document.getElementById("txtClave").value = ""
+    document.getElementById("txtDireccion").value = ""
+
     console.log(usuario);
 
 
 }
+
+
+function cargarPagina(){
+    document.getElementById("lblAlerta").style.display = "none"
+}
+
+cargarPagina()
